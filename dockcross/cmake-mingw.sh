433da234b0141ba9d53f542cd21d6f1ff8ed602c
Ion Agorria <ion@agorria.com>
2021-06-18 16:03:55 +0200

    Disable static builds by default

--- dockcross/cmake-mingw.sh
+++ dockcross/cmake-mingw.sh
@@ -1,4 +1,11 @@
-#!/bin/sh
+#!/bin/bash
+
+echo "Triplet: ${CROSS_TRIPLE}"
+if [[ $CROSS_TRIPLE == *shared ]]; then
+  LIB_SUFFIX=.dll.a
+else
+  LIB_SUFFIX=.a
+fi
 
 #rm -rf build/${CROSS_TRIPLE}
 
@@ -9,14 +16,17 @@ ${CROSS_TRIPLE}-cmake \
 -DBoost_INCLUDE_DIR="/usr/lib/mxe/usr/${CROSS_TRIPLE}/include" \
 -DOGG_INCLUDE_DIR="/usr/lib/mxe/usr/${CROSS_TRIPLE}/include" \
 -DVORBIS_INCLUDE_DIR="/usr/lib/mxe/usr/${CROSS_TRIPLE}/include" \
--DOGG_LIBRARY="/usr/lib/mxe/usr/${CROSS_TRIPLE}/lib/libogg.a" \
--DVORBIS_LIBRARY="/usr/lib/mxe/usr/${CROSS_TRIPLE}/lib/libvorbis.a" \
--DVORBISFILE_LIBRARY="/usr/lib/mxe/usr/${CROSS_TRIPLE}/lib/libvorbisfile.a" \
--DSDL2_INCLUDE_DIR="/usr/lib/mxe/usr/${CROSS_TRIPLE}/include" \
--DSDL2_LIBRARY="/usr/lib/mxe/usr/${CROSS_TRIPLE}/lib/libSDL2.a" \
+-DSDL2_INCLUDE_DIR="/usr/lib/mxe/usr/${CROSS_TRIPLE}/include/SDL2" \
+-DLIBRARY_SEARCH_PATHS="/usr/lib/mxe/usr/${CROSS_TRIPLE}/include" \
+-DOGG_LIBRARY="/usr/lib/mxe/usr/${CROSS_TRIPLE}/lib/libogg${LIB_SUFFIX}" \
+-DVORBIS_LIBRARY="/usr/lib/mxe/usr/${CROSS_TRIPLE}/lib/libvorbis${LIB_SUFFIX}" \
+-DVORBISFILE_LIBRARY="/usr/lib/mxe/usr/${CROSS_TRIPLE}/lib/libvorbisfile${LIB_SUFFIX}" \
+-DSDL2_LIBRARY="/usr/lib/mxe/usr/${CROSS_TRIPLE}/lib/libSDL2main.a;/usr/lib/mxe/usr/${CROSS_TRIPLE}/lib/libSDL2${LIB_SUFFIX}" \
 -DOPTION_DISABLE_STACKTRACE=On \
 $@
 
 cd build/${CROSS_TRIPLE}
 
 ninja -j$(nproc --all)
+
+exit 0
--- dockcross/run.sh
+++ dockcross/run.sh
@@ -5,6 +5,16 @@ export SSH_DIR=/noexist
 if [ ! -z $CLEAN ]; then
   rm ./dockcross/dockcross-*-perimeter
 fi
+if [ ! -f ./dockcross/dockcross-windows-shared-x86-perimeter ]; then
+  docker build -t dockcross-windows-shared-x86-perimeter -f ./dockcross/windows-shared-x86.Dockefile ./dockcross
+  docker run dockcross-windows-shared-x86-perimeter > ./dockcross/dockcross-windows-shared-x86-perimeter
+  chmod a+x ./dockcross/dockcross-windows-shared-x86-perimeter
+fi
+if [ ! -f ./dockcross/dockcross-windows-shared-x64-perimeter ]; then
+  docker build -t dockcross-windows-shared-x64-perimeter -f ./dockcross/windows-shared-x64.Dockefile ./dockcross
+  docker run dockcross-windows-shared-x64-perimeter > ./dockcross/dockcross-windows-shared-x64-perimeter
+  chmod a+x ./dockcross/dockcross-windows-shared-x64-perimeter
+fi
 if [ ! -f ./dockcross/dockcross-windows-static-x86-perimeter ]; then
   docker build -t dockcross-windows-static-x86-perimeter -f ./dockcross/windows-static-x86.Dockefile ./dockcross
   docker run dockcross-windows-static-x86-perimeter > ./dockcross/dockcross-windows-static-x86-perimeter
@@ -16,7 +26,11 @@ if [ ! -f ./dockcross/dockcross-windows-static-x64-perimeter ]; then
   chmod a+x ./dockcross/dockcross-windows-static-x64-perimeter
 fi
 
-echo "Building under windows x86_64"
-./dockcross/dockcross-windows-static-x64-perimeter ./dockcross/cmake-mingw.sh $@
-echo "Building under windows x86"
-./dockcross/dockcross-windows-static-x86-perimeter ./dockcross/cmake-mingw.sh $@
+echo "Building under windows shared x86"
+./dockcross/dockcross-windows-shared-x86-perimeter ./dockcross/cmake-mingw.sh $@
+echo "Building under windows shared x64"
+./dockcross/dockcross-windows-shared-x64-perimeter ./dockcross/cmake-mingw.sh $@
+#echo "Building under windows static x86"
+#./dockcross/dockcross-windows-static-x86-perimeter ./dockcross/cmake-mingw.sh $@
+#echo "Building under windows static x86_64"
+#./dockcross/dockcross-windows-static-x64-perimeter ./dockcross/cmake-mingw.sh $@
