934e7f4ed1fb61dbff67748bb1019ed7d0f3451d
Ion Agorria <ion@agorria.com>
2021-06-30 17:09:20 +0200

    dockcross stuff

--- docker/run.sh
+++ docker/run.sh
@@ -12,7 +12,7 @@ if [ ! -f $DOCKER_ROOT/dockcross-x86-perimeter ]; then
   docker run dockcross-x86-perimeter > $DOCKER_ROOT/dockcross-x86-perimeter
   chmod a+x $DOCKER_ROOT/dockcross-x86-perimeter
 fi
-if [ ! -f $DOCKER_ROOT/dockcross-x64-perimeter ]; then
+if [[ ! -z $BUILD_64 ]] && [[ ! -f $DOCKER_ROOT/dockcross-x64-perimeter ]]; then
   docker build -t dockcross-x64-perimeter -f $DOCKER_ROOT/dockcross-x64.Dockefile $DOCKER_ROOT
   docker run dockcross-x64-perimeter > $DOCKER_ROOT/dockcross-x64-perimeter
   chmod a+x $DOCKER_ROOT/dockcross-x64-perimeter
@@ -20,7 +20,7 @@ fi
 
 echo "Building under windows x86"
 $DOCKER_ROOT/dockcross-x86-perimeter $DOCKER_ROOT/dockcross-cmake.sh $@
-if [ ! -z $BUILD_64 ]; then
+if [[ ! -z $BUILD_64 ]]; then
   echo "Building under windows x64"
   $DOCKER_ROOT/dockcross-x64-perimeter $DOCKER_ROOT/dockcross-cmake.sh $@
 fi
