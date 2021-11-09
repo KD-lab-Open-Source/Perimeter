505a05eae81af0e044d9d49d33cc62c618911f03
Ion Agorria <ion@agorria.com>
2021-11-09 01:57:51 +0100

    Add asserts for float compliance and add stricter math

--- Source/XTool/xmath.cpp
+++ Source/XTool/xmath.cpp
@@ -15,6 +15,14 @@
 #pragma warning(disable : 4073 )
 #pragma init_seg(lib)
 
+//Assert that types are IEEE compilant
+#include <limits>
+static_assert(sizeof(float) == 4);
+static_assert(sizeof(double) == 8);
+static_assert(std::numeric_limits<float>::is_iec559);
+static_assert(std::numeric_limits<double>::is_iec559);
+static_assert(std::numeric_limits<long double>::is_iec559);
+
 ///////////////////////////////////////////////////////////////////////////////
 //
 //	Constants
