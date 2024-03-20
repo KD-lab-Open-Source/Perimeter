05591842655b0eea8ac5bc32dd893f0b544a5dae
Ion Agorria <ion@agorria.com>
2024-03-20 01:10:50 +0100

    meta: Bump version to 3.1.0

--- perimeter.nix
+++ perimeter.nix
@@ -51,7 +51,7 @@
   };
 in pkgs.stdenv.mkDerivation {
   pname = "perimeter";
-  version = "3.0.11";
+  version = "3.1.0";
   meta = with lib; {
     homepage = "https://github.com/KD-lab-Open-Source/Perimeter/";
     description = "Perimeter - A open-source RTS game from 2004 by K-D LAB";
