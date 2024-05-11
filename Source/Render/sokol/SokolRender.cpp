e49ab3949b0b8d47d7356aa491cc3c2ef1fb898f
Ion Agorria <ion@agorria.com>
2024-05-11 12:42:34 +0200

    sokol: Increase uniform_buffer_size to avoid crash in Metal (default is 4MB)

--- Source/Render/sokol/SokolRender.cpp
+++ Source/Render/sokol/SokolRender.cpp
@@ -76,6 +76,7 @@ int cSokolRender::Init(int xScr, int yScr, int mode, SDL_Window* wnd, int Refres
     desc.pipeline_pool_size = PERIMETER_SOKOL_PIPELINES_MAX,
     desc.shader_pool_size = 8,
     desc.buffer_pool_size = 1024 * 8;
+    desc.uniform_buffer_size = 1024 * 1024 * 32;
     desc.image_pool_size = 1024 * 4; //1024 is enough for PGW+PET game
     desc.logger.func = slog_func;
     
