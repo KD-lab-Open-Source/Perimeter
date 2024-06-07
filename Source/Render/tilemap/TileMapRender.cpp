001e2a666a4044e86aa761f47a8e2066e9df5bfc
Ivan Ushakov <ivan.ushakov@gmail.com>
2024-06-07 17:37:33 +0300

    One tile render for Direct3D and minor code fixes

--- Source/Render/tilemap/TileMapRender.cpp
+++ Source/Render/tilemap/TileMapRender.cpp
@@ -583,7 +583,7 @@ void cTileMapRender::DrawBump(cCamera* DrawNode,eBlendMode MatMode,TILEMAP_DRAW 
 
             if((!bumpTile->init) || Tile.GetUpdate() || update_line)
             {
-                bumpTile->Calc(!bumpTile->init || Tile.GetUpdate());
+                bumpTile->Calc(!shadow && (!bumpTile->init || Tile.GetUpdate()));
                 Tile.ClearUpdate();
             }
 
